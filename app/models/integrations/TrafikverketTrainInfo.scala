6220870058dae79e02fc188e77a2b07d2486eee6
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2013-03-02 18:18:38 +0200

    added departure list fetch for trafikverket

--- app/models/integrations/TrafikverketTrainInfo.scala
+++ app/models/integrations/TrafikverketTrainInfo.scala
@@ -1,22 +1,26 @@
 package models.integrations
 
-import play.api.libs.ws.{WS,Response}
+import play.api.libs.ws.{ WS, Response }
 import scala.concurrent.Future
 import scala.xml.XML
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.Play
 
 object TrafikverketTrainInfo {
-	lazy val apiKey = Play.current.configuration.getString("schedules.sj.apiKey").getOrElse("")
-	private lazy val stationList = WS.url("https://api.trafiklab.se/trafikverket/traininfo/stations/listAllCurrentlyUsed")
-	private def bodyToXml(response: Response) = XML.loadString(response.body)
+  lazy val apiKey = Play.current.configuration.getString("schedules.sj.apiKey").getOrElse("")
+  private lazy val stationList = WS.url("https://api.trafiklab.se/trafikverket/traininfo/stations/listAllCurrentlyUsed")
+  private def departureList(station: String) = WS.url(s"https://api.trafiklab.se/trafikverket/traininfo/stations/name/${station}/departures")
+  private def bodyToXml(response: Response) = XML.loadString(response.body)
 
-	lazy val fetchStationList = stationList.withQueryString("key" -> apiKey).get().map(bodyToXml _)
-/*
-
-	private lazy val trainList = WS.url("http://188.117.35.14/TrainRSS/TrainService.svc/AllTrains")
-	private def trainData(guid: String) = WS.url(s"http://188.117.35.14/TrainRSS/TrainService.svc/trainInfo?train=${guid}")
-
-	lazy val fetchTrainList = trainList.get().map(bodyToXml _)
-	def fetchTrainData(guid: String) = trainData(guid).get().map(bodyToXml _)*/
+  lazy val fetchStationList =
+    stationList
+      .withQueryString("key" -> apiKey)
+      .get()
+      .map(bodyToXml _)
+  
+  def fetchDepartureList(station: String) =
+    departureList(station)
+      .withQueryString("key" -> apiKey, "maxItems" -> "500", "maxHours" -> "48")
+      .get()
+      .map(bodyToXml _)
 }
--- test/models/integrations/TrafikverketTrainInfoSpec.scala
+++ test/models/integrations/TrafikverketTrainInfoSpec.scala
@@ -9,28 +9,54 @@ import play.api.test._
 import play.api.test.Helpers._
 
 class TrafikverketTrainInfoSpec extends Specification {
-	"Station list" should {
-		"be fetched asynchronously" in {
-			running(FakeApplication()) {
-				TrafikverketTrainInfo.fetchStationList should beAnInstanceOf[Future[_]]
-			}
-		}
-		"should contain stations as XML" in {
-			running(FakeApplication()) {
-			    for (stations <- TrafikverketTrainInfo.fetchStationList) {
-			      stations should \\("Station")
-			    }
-			}
-		}
-		"should have station names" in {
-			running(FakeApplication()) {
-			    for {
-			      stations <- TrafikverketTrainInfo.fetchStationList
-			      station <- stations \\ "Station"
-			    } {
-			      station should \("Namn")
-			    }
-			}
-		}
-	}
+  "Station list" should {
+    "be fetched asynchronously" in {
+      running(FakeApplication()) {
+        TrafikverketTrainInfo.fetchStationList should beAnInstanceOf[Future[_]]
+      }
+    }
+    "contain stations as XML" in {
+      running(FakeApplication()) {
+        for (stations <- TrafikverketTrainInfo.fetchStationList) {
+          stations should \\("Station")
+        }
+      }
+    }
+    "have station names" in {
+      running(FakeApplication()) {
+        for {
+          stations <- TrafikverketTrainInfo.fetchStationList
+          station <- stations \\ "Station"
+        } {
+          station should \("Namn")
+        }
+      }
+    }
+  }
+  
+  "Departure list" should {
+    val station = "Rinkeby"
+    "be fetched asynchronously" in {
+      running(FakeApplication()) {
+        TrafikverketTrainInfo.fetchDepartureList(station) should beAnInstanceOf[Future[_]]
+      }
+    }
+    "contain departures as XML" in {
+      running(FakeApplication()) {
+        for (departures <- TrafikverketTrainInfo.fetchDepartureList(station)) {
+          departures should \\("Trafiklage")
+        }
+      }
+    }
+    "have train ids for departing trains" in {
+      running(FakeApplication()) {
+        for {
+          departures <- TrafikverketTrainInfo.fetchDepartureList(station)
+          departure <- departures \\ "Trafiklage"
+        } {
+          departure should \("TagGrupp")
+        }
+      }
+    }
+  }
 }
