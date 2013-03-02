c4b034e27eb7ade7cc69974697ab734193bbc320
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2013-03-02 16:42:20 +0200

    fetching train list and individual train data for vr

--- app/models/schedules/VrTrainData.scala
+++ app/models/schedules/VrTrainData.scala
@@ -1,12 +1,15 @@
 package models.schedules
 
-import play.api.libs.ws.WS
+import play.api.libs.ws.{WS,Response}
 import scala.concurrent.Future
+import scala.xml.XML
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object VrTrainData {
-	import scala.concurrent.ExecutionContext.Implicits.global
+	private lazy val trainList = WS.url("http://188.117.35.14/TrainRSS/TrainService.svc/AllTrains")
+	private def trainData(guid: String) = WS.url(s"http://188.117.35.14/TrainRSS/TrainService.svc/trainInfo?train=${guid}")
+	private def bodyToXml(response: Response) = XML.loadString(response.body)
 
-	def fetchTrainList = {
-		Future {}
-	}
+	lazy val fetchTrainList = trainList.get().map(bodyToXml _)
+	def fetchTrainData(guid: String) = trainData(guid).get().map(bodyToXml _)
 }
--- test/models/schedules/VrTrainDataSpec.scala
+++ test/models/schedules/VrTrainDataSpec.scala
@@ -3,16 +3,31 @@ package test.models.schedules
 import org.specs2.mutable._
 import models.schedules.VrTrainData
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class VrTrainDataSpec extends Specification {
 	"Train list" should {
 		"be fetched asynchronously" in {
 			VrTrainData.fetchTrainList should beAnInstanceOf[Future[_]]
 		}
-		"contain RSS data" in {
+		"contain RSS items" in {
 			for (list <- VrTrainData.fetchTrainList) {
-				list should \("rss")
+				list should \\("item")
 			}
+		}
+		"have RSS items with train data" in {
+			for {
+			 	list <- VrTrainData.fetchTrainList
+			 	item <- list \\ "item"
+			} {
+				item should \("guid")
+			}
+		}
+	}
+	
+	"Train data" should {
+		"be fetched asynchronously" in {
+			VrTrainData.fetchTrainData("foo") should beAnInstanceOf[Future[_]]
 		}
 	}
 }
